7bde1b92d3a2f27346bd4c4a28e241648848b2f9
qvandekadsye <qvandekadsye@gmail.com>
2016-01-27 11:21:21 +0100

    classe client

--- src/online/Client.java
+++ src/online/Client.java
@@ -1,5 +1,63 @@
 package online;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 public class Client {
+	Socket as;
+	
+	public Client(String serverAddress,int port)
+	{
+		try {
+			this.as = new Socket(serverAddress, port);
+		} catch (UnknownHostException e) {
+			System.out.println("Error unknown host");
+			e.printStackTrace();
+		} catch (IOException e) {
+			System.out.println("I/O error");
+			e.printStackTrace();
+		} 
+	}
+	
+	public void read()
+	{
+		try
+		{
+			String s;
+			InputStream is = this.as.getInputStream();
+			InputStreamReader isr = new InputStreamReader(is);
+			BufferedReader br = new BufferedReader(isr);
+			while((s = br.readLine()) != null)
+			{
+				System.out.println(s);
+			}
+		}
+		catch(Exception e)
+		{
+			System.out.println("Error while reading\n"+e.getLocalizedMessage());
+		}
+	}
+	
+	public void write(String message)
+	{
+		try
+		{
+			OutputStream os = this.as.getOutputStream();
+			DataOutputStream dos = new DataOutputStream(os);
+			dos.writeBytes(message+"\n");
+			
+		}
+		catch(Exception e)
+		{
+			System.out.println("Error while reading\n"+e.getLocalizedMessage());
+		}
+		
+	}
 
 }
