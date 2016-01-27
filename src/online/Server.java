54d3d6f479ca4c509e5b684b78e3011fe0bb475c
Matthieu Caron <matthieu.caron@laposte.net>
2016-01-27 11:20:38 +0100

    travail server

--- src/online/Server.java
+++ src/online/Server.java
@@ -1,5 +1,27 @@
 package online;
 
-public class Server {
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
 
+public class Server {
+	
+	protected ServerSocket servsocket ;
+	
+	public Server(int port) throws IOException {
+		this.servsocket = new ServerSocket(port);
+	}
+	
+	public void launch() throws IOException {
+		while(true) {
+			Socket socket = this.servsocket.accept() ;
+			Thread thread = new Thread(new FTPprotocol(socket)) ;
+			thread.run() ;
+		}
+	}
+	
+	
+	
+	
+	
 }
