4249f6dd6540c46758813caf5549c9130d5c23d8
Matthieu Caron <matthieu.caron@laposte.net>
2016-02-10 10:37:55 +0100

    changement Etat

--- src/offline/Etat.java
+++ src/offline/Etat.java
@@ -1,7 +1,39 @@
 package offline;
 
-public enum Etat {
-	UNIDENTIFIED,
-	IDENTIFIED,
-	ANONYMOUS ;
+public class Etat {
+	
+	public enum StateEnum { UNIDENTIFIED ,IDENTIFIED, ANONYMOUS } ;
+	protected String pathname ;
+	protected boolean actif ;
+	protected StateEnum state ;
+	
+	public Etat(String pathname){
+		this.state = StateEnum.UNIDENTIFIED ;
+		this.actif = true ;
+		this.pathname = pathname ;
+	}
+	
+	public StateEnum getState() {
+		return this.state ;
+	}
+	
+	public boolean isActif() {
+		return this.actif ;
+	}
+	
+	public String getCurrentPath() {
+		return this.pathname ;
+	}
+	
+	public void setPathname(String path) {
+		this.pathname = path ;
+	}
+	
+	public void setState(StateEnum state) {
+		this.state = state ;
+	}
+	
+	public void changMod() {
+		this.actif = !this.actif ;
+	}
 }
